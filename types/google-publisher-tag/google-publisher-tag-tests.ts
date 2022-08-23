90db2957f067edf866a9314bcd49bc426c66a114
Nowhere007 <Nowhere007@users.noreply.github.com>
2022-08-23 15:44:33 +0000

    Sync latest changes to @types/google-publisher-tag

--- types/google-publisher-tag/google-publisher-tag-tests.ts
+++ types/google-publisher-tag/google-publisher-tag-tests.ts
@@ -1,2 +1,3 @@
-// Placeholder test file.
+// Tests for Google Publisher Tag 1.20220822
+// Synced from: https://github.com/googleads/google-publisher-tag/commit/fa6bf4f80acd50d39d344225d7fcfcf84d1b21d5
 googletag.cmd;
